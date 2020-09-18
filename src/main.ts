0bec274dee0cb9852fc08cbceed1acb513e5d504
ninja0911 <araisora30@gmail.com>
2020-09-18 22:16:38 +0700

    add webpack hmr configuration (#7)

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
+import { INestApplication } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
+
+  launchHotReload(app);
 }
+
 bootstrap();
+
+declare const module: any;
+function launchHotReload(app: INestApplication){
+  if (module.hot) {
+    module.hot.accept();
+    module.hot.dispose(() => app.close());
+  }
+}
+
+
